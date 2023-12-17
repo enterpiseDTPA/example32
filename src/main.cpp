2a9cf456ac5726a9591652cfacafd4545b438c3e
enterpriseDTPA <1551124967@qq.com>
2023-12-17 19:32:32 +0800

    feat:sub

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,9 @@
 
 #include "add.h"
 
+#include "sub.h"
 int main() {
     std::cout << "hello world" << std::endl;
     std::cout << "add(1, 2) = " << add(1, 2) << std::endl;
-    std::cout << "hhahahha";
+    std::cout << sub(1,2);
 }
